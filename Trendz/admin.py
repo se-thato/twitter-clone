e9c552c9f586dd13a09bbd081828390b41003d29
Thato <thatoselepe80@gmail.com>
2024-08-26 13:36:33 +0200

    add changes

--- Trendz/admin.py
+++ Trendz/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister group from your admin 
+admin.site.unregister(Group)
+
+#extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #displaying username fields on admin page
+    fields = ["username"]
+    inlines = ["ProfileInline"]
+
+
+#unregister initial user
+admin.site.unregister(User)
+
+#Reregister  user and Profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
+#combine Profile information together with the User information
+class ProfileInline(admin.StackedInline):
+    model = Profile
--- Trendz/models.py
+++ Trendz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#create a User Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #creating profile to be able to follow other profiles
+    follows = models.ManyToManyField("self",
+        related_name= "followed_by",
+        symmetrical=False, #this means you can be able to follow somebody and they don't have to follow you back
+        blank= True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
